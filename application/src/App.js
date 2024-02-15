1d20d78afd8d59291e572dc04bf015b184806b95
Collins Gichohi <cgichohi2018@gmail.com>
2024-02-14 18:28:32 -0800

    following video

--- application/src/App.js
+++ application/src/App.js
@@ -1,21 +1,7 @@
 import "./about.css";
 
-import { browserRouter, routes, route, BrowserRouter } from "react-router-dom";
-
-import { about } from "./pages/about";
-import { home } from "./pages/home";
-import { person1 } from "./pages/person1";
-
 function App() {
-  return (
-    <div>
-      <browserRouter>
-        <routes>
-          <route index element={<home />}></route>
-        </routes>
-      </browserRouter>
-    </div>
-  );
+  return <div className="about-page"></div>;
 }
 
 export default App;
