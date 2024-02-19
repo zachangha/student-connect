2d438469bed6e505354b1bb49851476fc8310aad
LucaT445 <luca.thilgen@gmail.com>
2024-02-18 20:36:58 -0800

    Adjusted image size

--- application/src/component/members/luke.js
+++ application/src/component/members/luke.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="bigBox">
       <div className="aboutMe">
-      <img src={lukeImage} alt="Luke Thilgen" /> 
+      <img src={lukeImage} alt="Luke Thilgen" width="200" height="200"/> 
       <h1> Luke Thilgen </h1>
       <h2> Back End Lead </h2>
       </div>
