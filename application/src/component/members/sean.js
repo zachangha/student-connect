95589421fd308f50719695c0a92e2939d30db8e8
Cross776 <seanibarradiaz@gmail.com>
2024-02-18 21:18:00 -0800

    removed a typo

--- application/src/component/members/sean.js
+++ application/src/component/members/sean.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles/aboutMe.css";
-import seanImage from "../images/sean.jpeg";
+import seanImage from "../images/sean.jpg";
 
 function App() {
   return (
