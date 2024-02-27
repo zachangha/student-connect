599e55bb949761e7c7ee1eb92a8bb4a47e0a7b91
Collins Gichohi <cgichohi2018@gmail.com>
2024-02-27 12:47:19 -0800

    added require instead of import

--- application/src/index.js
+++ application/src/index.js
@@ -1,11 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+const React = require("react");
+const ReactDOM = require("react-dom/client");
+require("./index.css");
+const App = require("./App").default;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  React.createElement(React.StrictMode, null, React.createElement(App, null))
 );
