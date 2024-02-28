021f8618832a07a29e74a0340716a0d84af853f6
Collins Gichohi <cgichohi2018@gmail.com>
2024-02-27 17:32:43 -0800

    reverted some things

--- application/src/index.js
+++ application/src/index.js
@@ -1,9 +1,11 @@
-const React = require("react");
-const ReactDOM = require("react-dom/client");
-require("./index.css");
-const App = require("./App").default;
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  React.createElement(React.StrictMode, null, React.createElement(App, null))
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
