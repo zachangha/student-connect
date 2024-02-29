67ebb89051ae6dd668b6edd6a869a6a64c59b5b5
Collins Gichohi <cgichohi2018@gmail.com>
2024-02-28 17:49:15 -0800

    working with express now

--- application/src/server.mjs
+++ application/src/server.mjs
@@ -12,10 +12,16 @@ const __dirname = fileURLToPath(new URL(".", import.meta.url));
 const root = path.resolve(__dirname, "..", "build");
 app.use(express.static(root));
 
+app.get("/members/:memberName", (req, res) => {
+  const memberName = req.params.memberName;
+  console.log(`Member requested: ${memberName}`);
+  res.sendFile(path.join(root, "index.html"));
+});
+
 app.use("/*", (req, res) => {
   res.sendFile(path.join(root, "index.html"));
 });
 
-app.listen(process.env.PORT || 5001, () => {
-  console.log("Server started on port " + (process.env.PORT || 5001));
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server started on port " + (process.env.PORT || 3000));
 });
