c6029013aa2193a7eee7b3dd8517bc6ff767c109
Collins Gichohi <cgichohi2018@gmail.com>
2024-03-12 12:53:03 -0700

    adding database stuff

--- application/src/server.mjs
+++ application/src/server.mjs
@@ -1,11 +1,13 @@
 import path from "path";
 import cors from "cors";
 import express from "express";
+import mongoose from "mongoose";
 import morgan from "morgan";
 import bodyParser from "body-parser";
 import { fileURLToPath } from "url";
 
 const app = express(); // create express app
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(morgan("dev"));
@@ -13,6 +15,15 @@ app.use(morgan("dev"));
 const __dirname = fileURLToPath(new URL(".", import.meta.url));
 const root = path.resolve(__dirname, "..", "build");
 app.use(express.static(root));
+
+mongoose.connect(process.env.DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.once("open", function () {
+  console.log("MongoDB connection successfully established");
+});
 
 app.get("/members/:memberName", (req, res) => {
   const memberName = req.params.memberName;
