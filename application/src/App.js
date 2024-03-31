f02c740b0f3d9bdf7d754c637ed8647c3ed0e712
Collins Gichohi <cgichohi2018@gmail.com>
2024-03-30 19:43:18 -0700

    adding sidbar navbar

--- application/src/App.js
+++ application/src/App.js
@@ -15,14 +15,66 @@ import Messages from "./component/messages.js";
 import Profile from "./component/profile.js";
 import Classes from "./component/classes.js";
 import Footer from "./component/footer.js";
+import V2Nav from "./component/v2Nav.js";
+import { createTheme } from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material/styles";
 
 function App() {
   return (
     <Router>
+      <ThemeProvider theme={themes}>
+        <V2Nav />
+      </ThemeProvider>
       <RoutesWrapper />
     </Router>
   );
 }
+
+// Create a theme instance.
+const themes = createTheme({
+  palette: {
+    primary: {
+      main: "#556cd6", // A blue shade
+    },
+    secondary: {
+      main: "#19857b", // A teal shade
+    },
+    error: {
+      main: "#ff1744", // A red shade
+    },
+    background: {
+      default: "#fff", // White background by default
+      paper: "#f5f5f5", // Light grey for paper elements
+    },
+  },
+  typography: {
+    fontFamily: ['"Roboto"', '"Helvetica"', '"Arial"', "sans-serif"].join(","),
+    h1: {
+      fontSize: "2.2rem",
+      fontWeight: 500,
+    },
+    body1: {
+      fontSize: "1rem",
+    },
+  },
+  components: {
+    // Customizing MUI components globally
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          borderRadius: "8px", // Rounded corners for buttons
+        },
+      },
+    },
+    MuiAppBar: {
+      styleOverrides: {
+        root: {
+          backgroundColor: "#556cd6", // Matching the primary color
+        },
+      },
+    },
+  },
+});
 
 function RoutesWrapper() {
   const location = useLocation();
@@ -30,7 +82,7 @@ function RoutesWrapper() {
 
   return (
     <>
-      {!authRoutes.includes(location.pathname) && <Navbar />}
+      {/* {!authRoutes.includes(location.pathname) && <Navbar />} */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/members/:memberName" element={<MemberPage />} />
