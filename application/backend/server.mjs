c21f74cd4f6093674637712d019c3d808f22ec91
Collins Gichohi <cgichohi2018@gmail.com>
2024-04-21 08:59:33 -0700

    got gemini api working

--- application/backend/server.mjs
+++ application/backend/server.mjs
@@ -10,7 +10,7 @@ import { fileURLToPath } from "url";
 import { connectToDatabase, insertUsers } from "./database.mjs";
 import User from "./models/User.mjs";
 import Class from "./models/Classes.mjs";
-import OpenAI from "openai";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const app = express();
 
@@ -27,32 +27,28 @@ app.use(express.static(root));
 
 connectToDatabase();
 
-// API endpoint for processing user messages
+// AI tutor set up
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.0-pro" });
+
+// Route for processing chat messages using the Gemini API
 app.post("/api/chat", async (req, res) => {
   try {
-    const { message } = req.body;
-    const apiKey = "YOUR_OPENAI_API_KEY";
-    const apiUrl =
-      "https://api.openai.com/v1/engines/davinci-codex/completions";
-    // Send user message to ChatGPT API
-    const response = await axios.post(
-      apiUrl,
-      {
-        prompt: "Hi is my API working?!",
-        max_tokens: 150,
-      },
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${apiKey}`,
-        },
-      }
-    );
-    // Extract and send the response from ChatGPT back to the frontend
-    res.status(200).json({ reply: response.data.choices[0].text.trim() });
-  } catch (error) {
-    console.error("Error:", error.message);
-    res.status(500).json({ error: "Something went wrong" });
+    // get user prompt from text field
+    const { prompt } = req.body;
+
+    // send prompt and wait for results
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    console.log(text);
+    res.status(200).json({ message: text });
+  } catch (err) {
+    console.error("API request failed with error: ", err);
+    res.status(500).json({
+      error: "Something went wrong",
+      details: err.message,
+    });
   }
 });
 
