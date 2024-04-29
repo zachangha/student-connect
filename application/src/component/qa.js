717f0beee59b03e50e8b0f7a76372981737d9ee2
Cross776 <seanibarradiaz@gmail.com>
2024-04-29 01:55:04 -0700

    title can be typed

--- application/src/component/qa.js
+++ application/src/component/qa.js
@@ -2,16 +2,25 @@ import React, { useState } from "react";
 import {
   Container,
   TextField,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemSecondaryAction,
   Button,
+  IconButton,
   Typography,
 } from "@mui/material";
 import "./styles/homePage.css";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 
 function App() {
-
+  // seting the constants
     const [questions, setQuestions] = useState([]);
     const [newQuestions, setNewQuestions] = useState("");
+
+    const [title, setTitle] = useState([]);
+    const [newTitle, setNewTitle] = useState("");
   
     const user = JSON.parse(localStorage.getItem("user", "userID", "karmaPoints") || "{}");
 
@@ -22,29 +31,58 @@ function App() {
 
     // adds questions. 
     //  later on user could select classes that user is in from drop down
+
+    const handleTitle = () => {
+      if (newTitle.trim() !== "") {
+        setTitle([...title, { id: Date.now(), text: newTitle, checked: false }]);
+        setNewTitle("");
+      }
+    };
     const handleAddQuestions = () => {
-        if (newQuestions.trim() !== "") {
+        if (newQuestions.trim() !== "")  {
           setQuestions([...questions, { id: Date.now(), text: newQuestions, checked: false }]);
           setNewQuestions("");
         }
       };
 
+      /*
+      const handleAddQuestions = () => {
+        if (newQuestions.trim() !== "") || (newTitle.trim() !== "") {
+          setQuestions([...questions, { id: Date.now(), text: newQuestions, checked: false }]);
+          setNewQuestions("");
+          setTitle([...title, { id: Date.now(), text: newTitle, checked: false }]);
+          setNewTitle("");
+        }
+      };
+      */
+      
+      // delete questions
+      const handleDeleteQuestions = (id) => {
+        setQuestions(questions.filter((question) => question.id !== id));
+      };
+
+      // user can input tilte and question, and post the question in a list
   return (
-    <><div className="Questions">
-          <h1> Questions and Answers </h1>
-          <h2> Title of Q and A</h2>
-      </div>
-      <div className="bigBox">
-      <h1> Questions PAGE </h1>
+    <><div className="bigBox">
+      <h1> Questions & Answers Page </h1>
     </div>
       <Container className="qaContainer">
               <TextField
+                  variant="outlined"
+                  label="Add Title"
+                  value={newTitle}
+                  onChange={(e) => setNewTitle(e.target.value)}
+                  fullWidth
+                  margin="normal" />
+
+                  <TextField
                   variant="outlined"
                   label="Add New Questions"
                   value={newQuestions}
                   onChange={(e) => setNewQuestions(e.target.value)}
                   fullWidth
                   margin="normal" />
+
               <Button
                   variant="contained"
                   color="primary"
@@ -54,9 +92,26 @@ function App() {
               >
                   Add Questions
               </Button>
-          // the question, maybe later on add a button to delete the question if you want
 
-
+              <List>
+          {questions.map((question) => (
+            <ListItem
+              key={question.id}
+              dense
+            >
+              <ListItemText primary={question.text} />
+              <ListItemSecondaryAction>
+                <IconButton
+                  edge="end"
+                  aria-label="delete"
+                  onClick={() => handleDeleteQuestions(question.id)}
+                >
+                  <DeleteIcon />
+                </IconButton>
+              </ListItemSecondaryAction>
+            </ListItem>
+          ))}
+        </List>
           </Container></>
 
   );
