f785aa92e36e91140c4f8a9df3086ac3596e867e
Collins Gichohi <cgichohi2018@gmail.com>
2024-05-03 18:39:07 -0700

    things are working

--- application/src/component/navbar.js
+++ application/src/component/navbar.js
@@ -112,7 +112,8 @@ export default function MiniDrawer() {
   // delete user info from local and redirect to landing page, sending alert
   const logout = () => {
     localStorage.removeItem("user");
-    setTimeout(() => navigate("/"), 600);
+    setTimeout(() => navigate("/", { replace: true }), 600);
+
     setAlert({
       open: true,
       message: "You have successfully logged out.",
